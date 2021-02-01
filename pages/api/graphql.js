f3354468768cceef2cc5d0d20f7785c342afa02f
Ian Perez <iperez@mit.edu>
2021-02-01 18:40:10 -0400

    Preliminary GraphQL API implementation

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,50 +1,132 @@
 import {ApolloServer, gql} from 'apollo-server-micro';
 import axios from 'axios';
+import {RESTDataSource} from "apollo-datasource-rest";
+
+class TmdbAPI extends RESTDataSource {
+    constructor(){
+        super();
+        this.baseURL = 'https://api.themoviedb.org/3/';
+    }
+    willSendRequest(request) {
+        request.params.set('api_key', process.env.API_KEY);
+    }
+    async getTrendingShows(){
+        console.log('TEST')
+        const result = await this.get('trending/tv/day');
+        return result.results;
+    }
+    async getPopularShows(){
+        const result = await this.get('tv/popular');
+        return result.results;
+    }
+    async getShowDetail(id) {
+        const result = await this.get(`tv/${id}`);
+        return result
+    }
+    async getSeason(id, season_number){
+        const result = await this.get(`tv/${id}/season/${season_number}`);
+        return result
+    }
+    async getEpisode(id, season_number, episode_number){
+        const result = await this.get(`tv/${id}/season/${season_number}/episode/${episode_number}`);
+        return result;
+    }
+}
 
 const typeDefs = gql`
-    type User {
+    
+    type Show {
         id: ID
-        login: String
-        avatar_url: String
+        name: String
+        poster_path: String
+        backdrop_path: String
+        genres: [String]
+        status: String
+        vote_average: Float
+        number_of_episodes: Int
+        number_of_seasons: Int
+        tagline: String
+        popularity: Float
+        seasons: [Season]
     }
+    
+    type Season {
+        id: ID
+        air_date: String
+        episode_count: Int
+        name: String
+        overview: String
+        poster_path: String
+        season_number: Int
+        episodes: [Episode]
+    }
+    
+    type Episode {
+        id: ID
+        air_date: String
+        episode_number: Int
+        name: String
+        overview: String
+        season_number: Int
+        still_path: String
+        vote_average: Float
+    }
+
     type Query {
-        getUsers: [User]
-        getUser(name: String!): User!
+        trendingShows: [Show]
+        popularShows: [Show]
+        show(id: ID): Show,
+        season(showId: ID, season_number: Int): Season
+        episode(showId: ID, season_number: Int, episode_number: Int): Episode
     }
 `
 
 const resolvers = {
     Query: {
-        getUsers: async () => {
-            try {
-                const users = await axios.get("https://api.github.com/users");
-                return users.data.map(({ id, login, avatar_url }) => ({
-                    id,
-                    login,
-                    avatar_url
-                }));
-            } catch (error) {
-                throw error;
-            }
+        trendingShows: async (_, __, {dataSources}) => {
+            return dataSources.tmdbAPI.getTrendingShows()
         },
-        getUser: async (_, args) => {
-            try {
-                const user = await axios.get(
-                    `https://api.github.com/users/${args.name}`
-                );
-                return {
-                    id: user.data.id,
-                    login: user.data.login,
-                    avatar_url: user.data.avatar_url
-                };
-            } catch (error) {
-                throw error;
-            }
+        popularShows: async (_, __, {dataSources}) => {
+            return dataSources.tmdbAPI.getPopularShows()
+        },
+        show: async (_, {id}, {dataSources}) => {
+            return dataSources.tmdbAPI.getShowDetail(id)
+        },
+        season: async(_, {id, season_number}, {dataSources}) => {
+            return dataSources.tmdbAPI.getSeason(id, season_number)
+        },
+        episode: async(_, {id, season_number, episode_number}, {dataSources}) => {
+            return dataSources.tmdbAPI.getEpisode(id, season_number, episode_number)
+        },
+    },
+    Show: {
+        genres: async (parent, _, {dataSources}) => {
+            return (await dataSources.tmdbAPI.getShowDetail(parent.id)).genres.map(item => item.name);
+        },
+        number_of_episodes: async (parent, _, {dataSources}) => {
+            return (await dataSources.tmdbAPI.getShowDetail(parent.id)).number_of_episodes;
+        },
+        number_of_seasons: async (parent, _, {dataSources}) => {
+            return (await dataSources.tmdbAPI.getShowDetail(parent.id)).number_of_seasons;
+        },
+        seasons: async(parent, _, {dataSources}) => {
+            return (await dataSources.tmdbAPI.getShowDetail(parent.id)).seasons.map(item => ({...item, showId: parent.id}));
+        }
+    },
+    Season: {
+        episodes: async (parent, _, {dataSources}) => {
+            return (await dataSources.tmdbAPI.getSeason(parent.showId, parent.season_number)).episodes
         }
     }
 }
 
-const apolloServer = new ApolloServer({typeDefs, resolvers});
+const apolloServer = new ApolloServer({
+    typeDefs,
+    resolvers,
+    dataSources: () => ({
+        tmdbAPI: new TmdbAPI(),
+    })
+});
 
 export const config = {
     api: {
